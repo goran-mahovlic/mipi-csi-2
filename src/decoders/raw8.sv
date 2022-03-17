c21944c88c18bdcc39ea31df2475b0490c7e7aa9
goran-mahovlic <goran.mahovlicgmail.com>
2022-03-17 12:54:31 +0100

    Fixing raw8 decoder

--- src/decoders/raw8.sv
+++ src/decoders/raw8.sv
@@ -1,7 +1,7 @@
 module raw8 (
-    input logic [7:0] image_data [3:0],
+    input logic [31:0] image_data,
     input logic image_data_enable,
-    output logic [7:0] raw [3:0],
+    output logic [31:0] raw,
     output logic raw_enable
 );
 assign raw = image_data;
