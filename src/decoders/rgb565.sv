87005b37838a383320eaa0779757c65849b219c2
goran-mahovlic <goran.mahovlicgmail.com>
2022-03-17 12:50:34 +0100

    Rewriting decoders to be able to compile

--- src/decoders/rgb565.sv
+++ src/decoders/rgb565.sv
@@ -1,15 +1,15 @@
 module rgb565 (
-    input logic [7:0] image_data [3:0],
+    input logic [31:0] image_data,
     input logic image_data_enable,
-    output logic [15:0] rgb [1:0],
+    output logic [31:0] rgb,
     output logic rgb_enable
 );
 
 logic [31:0] unpacked_image_data;
-assign unpacked_image_data = {image_data[3], image_data[2], image_data[1], image_data[0]};
+assign unpacked_image_data = {image_data[31:24], image_data[23:16], image_data[15:8], image_data[7:0]};
 
-assign rgb[0] = {unpacked_image_data[15:11], unpacked_image_data[10:5], unpacked_image_data[4:0]};
-assign rgb[1] = {unpacked_image_data[31:27], unpacked_image_data[26:21], unpacked_image_data[20:16]};
+assign rgb[15:0] = {unpacked_image_data[15:11], unpacked_image_data[10:5], unpacked_image_data[4:0]};
+assign rgb[31:16] = {unpacked_image_data[31:27], unpacked_image_data[26:21], unpacked_image_data[20:16]};
 assign rgb_enable = image_data_enable;
 
 endmodule
--- src/decoders/rgb888.sv
+++ src/decoders/rgb888.sv
@@ -1,9 +1,9 @@
 module rgb888 (
     input logic clock_p,
     input logic clock_n,
-    input logic [7:0] image_data [3:0],
+    input logic [31:0] image_data,
     input logic image_data_enable,
-    output logic [23:0] rgb [1:0],
+    output logic [47:0] rgb,
     output logic [1:0] rgb_enable
 );
 
@@ -13,23 +13,23 @@ module rgb888 (
 // 2 = RBGR -> RGBR
 logic [1:0] state = 2'd0;
 
-logic [7:0] last_upper_image_data [1:0] = '{8'd0, 8'd0};
+logic [31:0] last_upper_image_data;
 
 assign rgb_enable[0] = image_data_enable;
 assign rgb_enable[1] = image_data_enable && state == 2'd2;
 
-assign rgb[0] = state == 2'd0 ? {image_data[2], image_data[1], image_data[0]}
-    : state == 2'd1 ? {image_data[1], image_data[0], last_upper_image_data[3]}
-    : state == 2'd2 ? {image_data[0], last_upper_image_data[3], last_upper_image_data[2]};
+assign rgb[23:0] = state == 2'd0 ? {image_data[23:16], image_data[15:8], image_data[7:0]}
+                 : state == 2'd1 ? {image_data[15:8], image_data[7:0], last_upper_image_data[31:24]}
+                 : state == 2'd2 ? {image_data[7:0], last_upper_image_data[31:24], last_upper_image_data[23:16]};
 
-assign rgb[1] = {image_data[3], image_data[2], image_data[1]};
+assign rgb[47:24] = {image_data[31:24], image_data[23:16], image_data[15:8]};
 
 always @(posedge clock_p or posedge clock_n)
 begin
     if (image_data_enable)
     begin
         state <= state == 2'd2 ? 2'd0 : state + 2'd1;
-        last_upper_image_data <= image_data[3:2];
+        last_upper_image_data <= image_data[31:16];
     end
 end
 
